ea9fd0915a3485fbb668cc9bd9b0d330c45c70c9
Vivekanand P V <vivekanandpv@outlook.com>
2022-02-22 11:29:23 +0530

    done

---
+++ src/components/Playground.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Welcome {{name}}!</h3>
+    <p>Gender: {{gender}}</p>
+    <p>Position: {{position}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Playground",
+  data: function () {
+    return {
+      name: "Rajendra",
+      gender: "Male",
+      position: "Senior Developer",
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
